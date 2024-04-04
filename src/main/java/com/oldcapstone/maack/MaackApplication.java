e6524decfe62c3d6efef9c4fa20cdc05e69d31ff
swa07016 <swa07016@naver.com>
2024-04-04 22:37:32 +0900

    chore: 프로젝트 세팅

---
+++ src/main/java/com/oldcapstone/maack/MaackApplication.java
@@ -0,0 +1,13 @@
+package com.oldcapstone.maack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaackApplication.class, args);
+	}
+
+}
